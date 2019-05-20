4f032027e87cfd798226a5c44ce278239899daa2
alfano93 <david.alfano@rackspace.com>
2019-05-20 15:47:31 -0500

    updating script to actually use the environment variable file cephrc

--- scripts/prepare-deployment.sh
+++ scripts/prepare-deployment.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+source /opt/ceph-deployment-toolkit/cephrc
+
+echo " ################################################"
+echo " # DOWNLOADING ANSIBLE VERSION $ANSIBLE_VERSION #"
+echo " ################################################"
+
 add-apt-repository ppa:ansible/ansible
 apt update
 apt install ansible=$ANSIBLE_VERSION
 
-echo "CLONING CEPH-ANSIBLE VERSION $CEPH_ANSIBLE_VERSION"
+echo "######################################################"
+echo "# CLONING CEPH-ANSIBLE VERSION $CEPH_ANSIBLE_VERSION #"
+echo "######################################################"
 
 git clone https://github.com/ceph/ceph-ansible.git /opt/ceph-ansible
 
