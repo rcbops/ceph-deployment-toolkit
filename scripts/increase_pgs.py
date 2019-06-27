5f906deda63129c572bbf003c1d7ab56517e2b60
alfano93 <david.alfano@rackspace.com>
2019-06-27 11:08:47 -0500

    Change increase_pgs.py to accept command line input

--- scripts/increase_pgs.py
+++ scripts/increase_pgs.py
@@ -6,16 +6,29 @@ import re
 import time
 import datetime
 import sys
+import argparse
 
-misplaced_threshold = 2.0
-add_pgs = 1
-pool = 'volumes'
-max_pgs = 1024
+parser = argparse.ArgumentParser(description='Increase the number of placement groups of a pool in a ceph cluster')
+parser.add_argument("pool", help="STRING pool that you are increasing the number of pgs in", type=str)
+parser.add_argument("target_number", help="INT the number of pgs you want to end with",type=int)
+parser.add_argument("scale", help="INT the amount of pgs to add at each step",type=int)
+parser.add_argument("misplaced_max", help="FLOAT The percentage of objects that can be misplaced"type=float)
+
+args = parser.parse_args()
+
+misplaced_threshold = float(args.misplaced_max)
+add_pgs = int(args.scale)
+pool = str(args.pool)
+max_pgs = int(args.target_number)
 new_pgs = 0
 
+print(misplaced_threshold)
+print(add_pgs)
+print(pool)
+print(max_pgs)
 
 def get_pool_pgs( pool ):
-  pg_stats = json.loads( subprocess.check_output(["ceph", "osd", "pool", "get", pool, "pg_num", "-f", "json"]) )
+  pg_stats = json.loads( str(subprocess.check_output(["ceph", "osd", "pool", "get", pool, "pg_num", "-f", "json"])) )
   pool_pgs = int( pg_stats['pg_num'] )
   return pool_pgs
 
@@ -26,7 +39,7 @@ def set_pool_pgs( pool, pgs ):
 
 # get the current number of pgs in the target pool
 pool_pgs = get_pool_pgs( pool )
-print "PGS: " + str(pool_pgs)
+print("PGS: " + str(pool_pgs))
 sys.stdout.flush()
 
 while pool_pgs < max_pgs:
@@ -35,7 +48,7 @@ while pool_pgs < max_pgs:
   
   # check if overall health is not ERR
   if 'ERR' in health['overall_status']:
-    print "Exiting! Cluster is in HEALTH_ERR!"
+    print("Exiting! Cluster is in HEALTH_ERR!")
     exit(1)
 
   # if there are objects misplaced, wait until they're below the threshold
@@ -43,7 +56,7 @@ while pool_pgs < max_pgs:
     #print( json.dumps(health['checks']['OBJECT_MISPLACED']['summary']['message']) )
     m = re.search( '\((.*)%\)', health['checks']['OBJECT_MISPLACED']['summary']['message'] )
     misplaced = float( m.group(1) )
-    print "MISPLACED: " + str(misplaced)
+    print("MISPLACED: " + str(misplaced))
     sys.stdout.flush()
 
     # number of pgs to set the pool to
@@ -54,20 +67,20 @@ while pool_pgs < max_pgs:
 
     # if we're ready for an increase update the pg settings on the pool
     if misplaced < misplaced_threshold:
-      print str(datetime.datetime.now()) + " Below threshold.  Setting pool, " + pool + " pg_num and pgp_num to " + str(new_pgs)
+      print(str(datetime.datetime.now()) + " Below threshold.  Setting pool, " + pool + " pg_num and pgp_num to " + str(new_pgs))
       sys.stdout.flush()
       set_pool_pgs( pool, new_pgs )
     else:
-      print "Waiting, " + str(misplaced) + " > " + str(misplaced_threshold)
+      print("Waiting, " + str(misplaced) + " > " + str(misplaced_threshold))
       sys.stdout.flush()
   else:
-    print str(datetime.datetime.now()) + " No objects misplaced.  Setting pool, " + pool + " pg_num and pgp_num to " + str(new_pgs)
+    print(str(datetime.datetime.now()) + " No objects misplaced.  Setting pool, " + pool + " pg_num and pgp_num to " + str(new_pgs))
     sys.stdout.flush()
     set_pool_pgs( pool, new_pgs )
 
   # if we've hit the target quit
   if new_pgs == max_pgs:
-    print str(datetime.datetime.now()) + "Pool " + pool + " set to " + max_pgs + ".  Update complete"
+    print(str(datetime.datetime.now()) + "Pool " + pool + " set to " + str(max_pgs) + ".  Update complete")
     exit(0)
 
   pool_pgs = get_pool_pgs( pool )
