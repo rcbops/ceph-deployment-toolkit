cf08f2567fb87774c181e54b4141bce1f65f6e25
Matt Kleifgen <mattkleifgen@users.noreply.github.com>
2019-09-03 09:24:45 -0500

    adding script to discover and fix (#34)
    rotational indicator in sysfs

---
+++ scripts/ssd_rotational_fix.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+UDEV_RULE_FILE="/etc/udev/rules.d/60-ssd-nonrotational.rules"
+
+VENDOR="$(dmidecode |grep Vendor |awk '{print $2}')"
+
+# Dell
+if [ "${VENDOR}" = "Dell" ]
+    for VDISK in $(omreport storage vdisk |awk '/^ID/ {print $3}');
+    do
+        PDISKS=$(omreport storage pdisk controller=0 vdisk=${VDISK} |awk -F: '/^ID/ {print $4}' |wc -l);
+        if (( $PDISKS == 1 ))
+        then
+            MEDIA=$(omreport storage vdisk controller=0 vdisk=${VDISK} |awk '/^Media/ {print $3}')
+            if [ "${MEDIA}" = "SSD" ]
+            then
+                BLKDEV=$(omreport storage vdisk controller=0 vdisk=${VDISK} |awk -F\/ '/^Device Name/ {print $NF}')
+                echo "# device ${BLKDEV} set rotational for SSD" >> ${UDEV_RULE_FILE}
+                SCSIDEV=$(ls /sys/block/${BLKDEV}/device/scsi_device/)
+                echo "ACTION==\"add|change\", SUBSYSTEM==\"block\", KERNELS==\"${SCSIDEV}\", ATTR{queue/rotational}=\"0\"" >> ${UDEV_RULE_FILE}
+            fi
+        fi
+    done
+fi
